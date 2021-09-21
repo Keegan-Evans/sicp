ef085e340b809efd2f3e7f0f497050d2c1705531
Keegan Evans <keegan.evans@gmail.com>
2021-09-20 17:08:14 -0700

    EX: 2.1

---
+++ ch2/exercises.scm
@@ -0,0 +1,9 @@
+; 2.1
+(define (ex2.1)
+  (define (make-rat n d)
+      (cond ((and (< n 0) (< d 0))
+             (cons (abs n) (abs d)))
+            ((or (< n 0)(< d 0))
+             (cons (- (abs n)) (abs d)))
+            (else (cons n d))))
+  (lambda (x y) (make-rat x y)))
--- ch2/rational_arithmetic.scm
+++ ch2/rational_arithmetic.scm
@@ -9,5 +9,26 @@
   (display (denom x))
   (newline))
 
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rt (- (* (numer x) (denom y))
+              (* (numer y) (denom x)))
+           (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (numer y) (denom x))))
+
+(define (equal-rat x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+; EXAMPLE CODE
 (define example-num (make-rat 3 4))
-(print-rat example-num)
